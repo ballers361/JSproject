e3db24764575b6bc01bbf7685af70e587a6b0e5d
msuthar5 <msuthar@hawk.iit.edu>
2016-12-05 12:00:22 -0600

    fixed function to return userID upon search

--- js/site.js
+++ js/site.js
@@ -1,12 +1,16 @@
 (function($) {
   $(document).ready(
-    function() {
+  function() {
+    var uName, spotify_API_URL, spotify_client_ID, spotify_search_URL;
+
   $('#sp-form').on('submit', function(event) {
-      var uName = $('#sp-username').val();
-      var spotify_API_URL = 'https://api.spotify.com/v1/';
-      var spotify_client_ID = '09c1185efbd44df3a3a7c566b1878f40';
+      uName = $('#sp-username').val();
+      spotify_API_URL = 'https://api.spotify.com/v1/';
+      spotify_client_ID = '09c1185efbd44df3a3a7c566b1878f40';
       var spotify_client_secret = 'c1c16dda17f341e0a973f8c1b0fd6bea';
-      var spotify_search_URL = 'https://api.spotify.com/v1/search?q='+ uName +'type=artist&limit=1';
+      spotify_search_URL = 'https://api.spotify.com/v1/search?q='+ uName +'&type=artist&limit=1';
+      console.log(uName);
+      console.log(spotify_search_URL);
 
       $.get(
         spotify_search_URL, function(data) {
@@ -16,14 +20,7 @@
           event.preventDefault();
       });
   });
-/*$.get(
-  'https://api.spotify.com/v1/search?q=kanye%20west&type=artist&limit=1',
-  function(data) {
-    $('#primary').append(
-    '<p>' + data.artists.items[0].id + '</p>'
-    );
-  });
-*/
-        });
+
+});
 
 })(jQuery);
